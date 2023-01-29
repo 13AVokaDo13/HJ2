83d9bf47a8e381034c0b39fae84d40d249cab018
Sofin Rodion <13avokado13@gmail.com>
2023-01-29 12:36:49 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int bonus = 20;
+        int price = 8753;
+        int result = price / bonus;
+        System.out.println("бонусные мили:");
+        System.out.println(result);
+    }
+}
